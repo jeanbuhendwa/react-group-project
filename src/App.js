f15c746448d1c6d13e0b5d379bf8ebe8072d6c9f
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-04-11 11:20:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import LayOut from './components/layout';
+import Mission from './pages/mission';
+import Profile from './pages/profile';
+import Rocket from './pages/rocket';
 
 function App() {
-  return <h1>Heloooo</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<LayOut />}>
+        <Route index element={<Rocket />} />
+        <Route path="missions" element={<Mission />} />
+        <Route path="profile" element={<Profile />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
