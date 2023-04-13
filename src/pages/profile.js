f3b8506e4fa9c602ea06bae9f000b06834083b14
Patrick Mukula <Patrickkeita80@yahoo.com>
2023-04-13 12:45:13 -0700

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,9 +1,8 @@
 import { useSelector } from 'react-redux';
-import MissionProfile from './missionProfile';
 
 const Profile = () => {
   const rockets = useSelector((state) => state.rocket.rockets.filter((rocket) => rocket.reserved));
-
+const missions = useSelector((state) => state.mission.missions.filter((mis) => mis.joined));
   return (
     <main className="mainProfile">
       <section className="reservedRocket">
@@ -19,6 +18,21 @@ const Profile = () => {
             ))
           ) : (
             <p>No rockets reserved.</p>
+          )}
+        </ul>
+      </section>
+<section className="missionJoined">
+        <h2>My Missions</h2>
+        <ul>
+          {missions.length > 0 ? (
+            missions.map((mission) => (
+              <li key={mission.id} className="misJoined">
+                <h2>{mission.mission_name}</h2>
+                <p>{mission.description}</p>
+              </li>
+            ))
+          ) : (
+            <p>No Mission Joined.</p>
           )}
         </ul>
       </section>
